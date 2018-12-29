1b2e551739e6adff3007baa11984b0db7d66fca8
Fernando Garcia <fdo.garcia.vega@gmail.com>
2018-12-29 20:22:50 +0100

    update nuget packages

--- nuget/BuildNuGet.bat
+++ nuget/BuildNuGet.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 CLS
 
-..\source\.nuget\nuget Pack iTin.Export.Core.1.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
-..\source\.nuget\nuget Pack iTin.Export.Writers.Adobe.1.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
-..\source\.nuget\nuget Pack iTin.Export.Writers.OpenXml.DocX.1.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
-..\source\.nuget\nuget Pack iTin.Export.Writers.OpenXml.Xlsx.1.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\source\.nuget\nuget Pack iTin.Export.Core.2.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\source\.nuget\nuget Pack iTin.Export.Writers.Adobe.2.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\source\.nuget\nuget Pack iTin.Export.Writers.OpenXml.DocX.2.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\source\.nuget\nuget Pack iTin.Export.Writers.OpenXml.Xlsx.2.0.0.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
 
 
 
