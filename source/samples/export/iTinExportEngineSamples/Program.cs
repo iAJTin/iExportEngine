d696b833ae00f7e306820c5806fa40201b2635bb
Fernando Garcia <fdo.garcia.vega@gmail.com>
2019-01-23 08:20:46 +0100

    samples

---
+++ source/samples/export/iTinExportEngineSamples/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace iTinExportEngineSamples
+{
+    using System;
+    using System.Diagnostics;
+
+    class Program
+    {
+        private const string LastStepText = " Finished without errors. Press any key...";
+
+        private static readonly Stopwatch Watch = new Stopwatch();
+
+        private static TimeSpan _sample9XTime;
+
+        static void Main(string[] args)
+        {
+            Watch.Start();
+            // Sample 1 - simply creates a new workbook from scratch
+            // containing a worksheet that adds a few numbers together 
+            Sample01.RunFromConfigurationFileSample();
+            _sample9XTime = Watch.Elapsed;
+            Watch.Stop();
+
+            WriteElapsedTime(_sample9XTime);
+            Console.WriteLine(LastStepText);
+            Console.ReadKey();
+        }
+
+        private static void WriteElapsedTime(TimeSpan ts1)
+        {
+            var totalTime = ts1;
+            Console.WriteLine();
+            Console.WriteLine(@"					Elapsed Time");
+            Console.WriteLine(new string('=', 52));
+            Console.WriteLine(@" Samples 9x				{0:00}:{1:00}.{2:00}", ts1.Minutes, ts1.Seconds, ts1.Milliseconds / 10);
+            Console.WriteLine(new string('-', 52));
+            Console.WriteLine(@"					{0:00}:{1:00}.{2:00}", totalTime.Minutes, totalTime.Seconds, totalTime.Milliseconds / 10);
+            Console.WriteLine(new string('=', 52));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/samples/export/iTinExportEngineSamples/Sample01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using iTin.Export;
+using iTin.Export.Inputs;
+using iTin.Export.Model;
+
+namespace iTinExportEngineSamples
+{
+    public class Sample01
+    {
+        private const string EpplusHeader          = " 9 - EPPlus samples";
+        private const string OneStepText           = " a. Read data from xml file";
+        private const string TwoStepText           = " b. Export data";
+        private const string ThreeStepText         = "   . MS Excel (xlsx)";
+        private const string FourStepText          = "     - From code";
+        private const string FiveStepText          = "     - From configuration file";
+        private const string FirstSampleStepText   = "         -> Some data...                   -> [sample90.xlsx] <-";
+        private const string SecondSampleStepText  = "         -> ... with chart ...             -> [sample90-chart.xlsx] <-";
+        private const string ThirdSampleStepText   = "         -> ... and banner ...             -> [sample90-banner.xlsx] <-";
+        private const string FourthSampleStepText  = "         -> ... and resume info ...        -> [sample90-headerinfo.xlsx] <-";
+        private const string FifthSampleStepText   = "         -> ... and built-in functions ... -> [sample90-headerinfo-built-in-functions.xlsx] <-";
+        private const string SixthSampleStepText   = "         -> ... and custom functions       -> [sample90-headerinfo-custom-functions.xlsx] <-";
+
+        /// <summary>
+        /// Runs the sample.
+        /// </summary>
+        public static void RunFromConfigurationFileSample()
+        {
+            Console.WriteLine(EpplusHeader);
+            Console.WriteLine(new string('-', 52));
+
+            Console.WriteLine(OneStepText);
+            //var inputUri = new Uri(Properties.Settings.Default.SEKRatesXmlInput, UriKind.Relative);
+            //var export = new XmlInput(inputUri);
+
+            var inputUri = new Uri(Properties.Settings.Default.InventoryXmlInput, UriKind.Relative);
+            var export = new XmlInput(inputUri);
+
+            //Console.WriteLine(OneStepText);
+            //var inputUri = new Uri(Properties.Settings.Default.Sample90, UriKind.Relative);
+            //var export = new XmlInput(inputUri);
+
+            Console.Write(TwoStepText);
+            Console.WriteLine();
+            Console.WriteLine(ThreeStepText);
+
+            Console.WriteLine(FiveStepText);
+            Console.WriteLine(FirstSampleStepText);
+            //var sample90FileConfiguration = new Uri(Properties.Settings.Default.Sample16, UriKind.Relative);
+            //export.Export(ExportSettings.ImportFrom(sample90FileConfiguration, "sample16"));
+
+            var sample90FileConfiguration = new Uri(Properties.Settings.Default.Sample01Configuration, UriKind.Relative);
+            export.Export(ExportSettings.ImportFrom(sample90FileConfiguration));
+        }
+    }
+}
